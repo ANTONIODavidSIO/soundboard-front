471fed493767473a86787c4327a436354268aa20
Antonio David <david.antonio.sio76@gmail.com>
2024-03-03 10:52:32 +0100

    original

--- src/App.js
+++ src/App.js
@@ -1,32 +1,12 @@
 import React, { useState } from 'react';
-import ReactDOM from 'react-dom';
-import axios from 'axios';
+import Routes from './config/routes';
 
 const App = () => {
-  const [file, setFile] = useState(null);
+  
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    const formData = new FormData();
-    formData.append('file', file);
-
-    axios.post('/api/upload', formData)
-      .then(res => {
-        console.log(res.data);
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  };
-
-  return (
-    <div>
-      <input type="file" onChange={(e) => setFile(e.target.files[0])} />
-      <button onClick={handleSubmit}>Upload</button>
-    </div>
+return (
+    <Routes/>
   );
-};
+}
+export default App;
 
-const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
